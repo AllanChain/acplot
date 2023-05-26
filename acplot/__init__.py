fb5eebcc3664327671d0aab6ffeef8790549f71a
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-05-26 08:57:12 +0800

    allow saving multiple image formats

--- acplot/__init__.py
+++ acplot/__init__.py
@@ -1,24 +1,40 @@
+from __future__ import annotations
+
 from contextlib import contextmanager
-from matplotlib import pyplot as plt
 from pathlib import Path
+
 import scienceplots  # NOQA
+from matplotlib import pyplot as plt
 
 save_dir = Path(".")
 
 plt.style.use(["science", Path(__file__).parent / "default.mplstyle"])
 
 
+def savefig(name: str, img_format: str = "pdf"):
+    file_path = save_dir / f"{name}.{img_format}"
+    metadata = {}
+    if img_format == "pdf":
+        metadata["CreationDate"] = None
+    plt.savefig(file_path, metadata=metadata)
+
+
 @contextmanager
-def figure(name: str, save=False, figsize=(8, 6), **kwargs) -> None:
+def figure(
+    name: str,
+    save: str | list[str] | bool = False,
+    figsize: tuple[int, int] | None = None,
+    **kwargs,
+) -> None:
     plt.close(name)
     yield plt.figure(name, figsize=figsize, **kwargs)
     if save:
-        plt.savefig(
-            save_dir / f"{name}.pdf",
-            dpi=150,
-            metadata={"CreationDate": None},
-            bbox_inches="tight",
-        )
+        if save is True:
+            save = ["pdf"]
+        elif isinstance(save, str):
+            save = [save]
+        for img_format in save:
+            savefig(name, img_format)
     plt.show()
 
 
